4ec4c2b50526dd077dde74aff61702707438e674
Jasper012606 <japeemga@gmail.com>
2025-03-07 19:16:32 +0800

    Update prog8.py

--- prog8.py
+++ prog8.py
@@ -1,2 +1,2 @@
 # A program that asks the user to enter 10 numbers and prints the number of odd numbers
-print(sum(int(input("Enter a number: ")) % 2 == 1 for _ in range(10)))
+print(sum(int(input("Enter a number: ")) % 2 == 1 for i in range(10)))
