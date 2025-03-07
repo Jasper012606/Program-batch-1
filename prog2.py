fbc67b6fd6a15412965b2cc894198e7909b5dc81
Jasper012606 <japeemga@gmail.com>
2025-03-07 19:19:24 +0800

    Update prog2.py

---
+++ prog2.py
@@ -0,0 +1,3 @@
+# A program that asks the user to input 2 numbers and prints "Equal" if the two numbers are the same
+if int(input("Enter a number: ")) == int(input("Enter another number: "))
+  print("Equal")
