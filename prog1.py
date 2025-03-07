c34208d26ba2e6fd9d37567a4ea609c1bed57665
Jasper012606 <japeemga@gmail.com>
2025-03-07 19:20:56 +0800

    Update prog1.py

---
+++ prog1.py
@@ -0,0 +1,7 @@
+#A program that asks user to input 2 numbers and prints the bigger number
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+if num1 > num2
+  print("The bigger number is", num1)
+else
+  print("The bigger number is", num2)
